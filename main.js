d5caf6583ba9be068a8dd9fbd92ff0603189f13d
MMosman <moustafamohamedoosman@gmail.com>
2025-02-19 23:42:31 +0200

    Adding full screen feature to all images

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//^ Scoll to top
 let scrollToTopBtn = document.querySelector(".scroll-to-top");
 window.onscroll = () => {
     if(window.scrollY >= 600) {
@@ -16,37 +17,27 @@ scrollToTopBtn.onclick = () => {
 }
 
 // ---------------------------------------------------------------------------
-
+//^ Image slider parts
 function initialSlider(slider) {
     const sliderImages = Array.from(slider.querySelectorAll('.slider-container img'));
     const sliderCount = sliderImages.length;
     let currentSlide = 1;
-
     let slideElementNumber = slider.querySelector(".slide-number");
-
     let prevButton = slider.querySelector('.prev');
     let nextButton = slider.querySelector('.next');
     let indicatorsContainer = slider.querySelector('.indicators');
-
     let paginationElement = document.createElement("ul");
-
     paginationElement.setAttribute("class", "pagination-ul");
-
     for (let index = 1; index <= sliderCount; index++) {
         let paginationItem = document.createElement('li');
         paginationItem.setAttribute("data-index", index);
         paginationItem.appendChild(document.createTextNode(index));
-
         paginationElement.appendChild(paginationItem);
     }
-
     slider.querySelector(".indicators").appendChild(paginationElement);
-
     let paginationCreatedUl = slider.querySelector(".pagination-ul");
-
     //Get pagination items
     let paginationBullets = Array.from(slider.querySelectorAll('.pagination-ul li'))
-
     //Loop through all bullets items
     paginationBullets.forEach(function (bullet) {
     bullet.addEventListener("click", function () {
@@ -54,14 +45,9 @@ function initialSlider(slider) {
         theChecker();
         });
     });
-
-
-    
     theChecker();
     nextButton.onclick = nextSlide;
     prevButton.onclick = prevSlide;
-
-
     function nextSlide () {
         if(nextButton.classList.contains('disabled')) {
             return 0;
@@ -70,7 +56,6 @@ function initialSlider(slider) {
             theChecker();
         }
     }
-
     function prevSlide () {
         if(prevButton.classList.contains('disabled')) {
             return 0;
@@ -79,21 +64,23 @@ function initialSlider(slider) {
             theChecker();
         }
     }
-
     function theChecker () {
-
         //Set the slider number
         slideElementNumber.textContent = "Image " + currentSlide + " of " + sliderCount;
-
         //Remove All Active Classes
         removeAllActive();
-
         //Set Active class on the current slide
         sliderImages[currentSlide - 1].classList.add('active');
+        sliderImages[currentSlide - 1].style.cssText = "z-index: 2; display: block; opacity: 0;";
+        setTimeout(() => {
+            sliderImages[currentSlide - 1].style.opacity = "1";
+        }, 200);
 
+        setTimeout( () => {
+            sliderImages[currentSlide - 1].style.visibility = "visible";
+        },1000);
         //Set Active class on the pa
         paginationCreatedUl.children[currentSlide -1].classList.add('active');
-
         //Check if the current is the first
         if(currentSlide === 1) {
             //Add disabled class on the prev button
@@ -102,7 +89,6 @@ function initialSlider(slider) {
             //remove disabled class from the pre button
             prevButton.classList.remove("disabled");
         }
-
         //Check if the current is the Latest
         if(currentSlide === sliderCount) {
             //Add disabled class on the next button
@@ -112,24 +98,45 @@ function initialSlider(slider) {
             nextButton.classList.remove("disabled");
         }
     }
-
     //Remove All Active Classes
     function removeAllActive () {
-
         //Loop Through Images
         sliderImages.forEach(function(img) {
             img.classList.remove('active');
+            img.style.cssText = "z-index: 0; display: none";
         });
-
         //Loop Through pagination
         paginationBullets.forEach(function(bullet) {
             bullet.classList.remove('active');
         })
     }
-
 }
 // Initialize All Sliders
 document.querySelectorAll('.slider-wrapper').forEach(initialSlider);
 
-
 // ---------------------------------------------------------------------------
+//^ Full screen when the user click on the image
+
+
+const popupImg = document.querySelector(".popup-img");
+const popupClose = document.querySelector(".slider-wrapper .popup-img span");
+
+document.querySelectorAll(".slider-container img").forEach(image => {
+    image.onclick = () => {
+        document.querySelector(".popup-img img").src = image.getAttribute("src");
+        popupImg.classList.add("showBlock");
+        // تشغيل التأثير بعد لحظة
+        setTimeout(() => {
+            popupImg.classList.add("showOpacity");
+        },100);
+        document.body.classList.add("no-scroll");
+    };
+});
+
+popupClose.onclick = () => {
+    setTimeout( () => {
+        popupImg.classList.remove("showOpacity");
+    },10);
+    popupImg.classList.remove("showBlock");
+    document.body.classList.remove("no-scroll");
+};
